4d7830c974e194a7f903c87453d21fefff47677f
butterbeetle <butterbeetle@naver.com>
2024-05-23 21:06:05 +0900

    Style: 사용하지 않는 Context 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import RecordContextProvider from "./context/RecordContext";
 import store from "./redux/store/store";
 import router from "./routes/router";
 
 export default function App() {
   return (
-    <RecordContextProvider>
-      <Provider store={store}>
-        <RouterProvider router={router} />
-      </Provider>
-    </RecordContextProvider>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   );
 }
